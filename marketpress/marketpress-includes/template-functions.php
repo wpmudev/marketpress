5c653b131775fe9dcb46a780d58beee6e8bbebdc
aaron <aaron@f53fe46e-8c02-459b-9fdd-93e75771b278>
2013-03-22 22:19:39 +0000

    - Change Shipping form label when shipping is turned off
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/marketpress@8867 f53fe46e-8c02-459b-9fdd-93e75771b278

--- marketpress/marketpress-includes/template-functions.php
+++ marketpress/marketpress-includes/template-functions.php
@@ -548,7 +548,7 @@ function _mp_cart_shipping($editable = false, $echo = false) {
 
     $content .= '<table class="mp_cart_shipping">';
     $content .= '<thead><tr>';
-    $content .= '<th colspan="2">'.($mp->download_only_cart($mp->get_cart_contents() && !$mp->global_cart) ? __('Enter Your Checkout Information:', 'mp') : __('Enter Your Shipping Information:', 'mp')).'</th>';
+    $content .= '<th colspan="2">'.(($mp->download_only_cart($mp->get_cart_contents() && !$mp->global_cart) ||  $mp->get_setting('shipping->method') != 'none') ? __('Enter Your Checkout Information:', 'mp') : __('Enter Your Shipping Information:', 'mp')).'</th>';
     $content .= '</tr></thead>';
     $content .= '<tbody>';
     $content .= '<tr>';
