99f15be725338cda52c2b60d96389e3cfccfe1ba
Jonathan Cowher <jon@incsub.com>
2014-10-06 20:24:38 -0400

    Initial commit for product variation image/description swapouts

--- ui/js/frontend.js
+++ ui/js/frontend.js
@@ -22,8 +22,8 @@ var marketpress = {};
 	}
 	
 	marketpress.initSelect2 = function(){
-		$('.mp_list_filter').find('select').select2({
-			"dropdownAutoWidth" : true,
+		$('.mp_select2').select2({
+			"dropdownAutoWidth" : 1,
 			"minimumResultsForSearch" : -1	// hide the search box
 		});
 	}
@@ -43,11 +43,26 @@ var marketpress = {};
 			}, 300);
 		});
 	}
+	
+	marketpress.initToggleVariations = function(){
+		$('.mp_variant_image_link').mouseover(function(e){
+			e.preventDefault();
+			
+			var $this = $(this),
+					newSrc = $this.find('.mp_variant_alt_image').find('img').attr('src'),
+					newText = $this.find('.mp_variant_alt_content').html();
+			
+			$this.addClass('selected').siblings().removeClass('selected');
+			$('.mp_product_image_link').attr('href', $this.attr('href')).find('img').attr('src', newSrc);
+			$('.mp_product_content_text').html(newText);
+		});
+	}
 }(jQuery));
 
 jQuery(document).ready(function(){
 	marketpress.initSelect2();
 	marketpress.initProductOptionsMenu();
+	marketpress.initToggleVariations();
 });
 
 window.onload = function(){
