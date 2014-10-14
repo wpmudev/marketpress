15fa4bc5fda3da561996bd443f38f7ba24955ee9
Jonathan Cowher <jon@incsub.com>
2014-10-14 13:19:03 -0400

    Initial commit for PHPUnit tests

---
+++ tests/test-mp-product.php
@@ -0,0 +1,15 @@
+<?php
+
+class MP_Product_Tests extends WP_UnitTestCase {
+	function test_in_stock() {
+		$product = $this->factory->post->create_and_get( array( 'post_type' => 'product' ) );
+		$variation = $this->factory->post->create_and_get( array( 'post_type' => 'mp_product_variation', 'post_parent' => $product->ID ) );
+		
+		add_post_meta( $variation->ID, '_inventory', 'WPMUDEV_Field_Text' );
+		add_post_meta( $variation->ID, 'inventory', 1);
+		
+		$variation = new MP_Product( $variation );
+		$this->expectOutputString( '1' );
+		echo $variation->in_stock( 2 );
+	}
+}
