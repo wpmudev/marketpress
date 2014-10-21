b847ca2ddbd9dc82b6a41b2634dffddeaa1671fb
Jonathan Cowher <jon@incsub.com>
2014-10-21 18:38:15 -0400

    Update mp_cart.js
    - Add phpdoc comments
    - Make so that single product template form can reuse existing code

--- ui/js/mp-cart.js
+++ ui/js/mp-cart.js
@@ -2,12 +2,60 @@
 var mp_cart = {};
 
 (function($){
+	/**
+	 * Refers to the validation args for the add-to-cart/buy-now form
+	 *
+	 * @since 3.0
+	 * @type object
+	 */
+	mp_cart.productFormArgs = {
+		"errorClass" : "mp_input_error",
+		"errorElement" : "span",
+		"errorPlacement" : function(error, element){
+			error.appendTo(element.closest('.mp_product_options_att').find('.mp_product_options_att_label'));
+		},
+		"ignore" : "",
+		"submitHandler" : function(form){
+			var $form = $(form),
+					$loadingGraphic = $('#cboxLoadingOverlay'),
+					item = {},
+					qty = $form.find('[name="product_quantity"]').val();
+					
+			// Build item object
+			item['product_id'] = $form.find('[name="product_id"]').val();
+			$form.find('[name^="product_attr_"]').filter(':checked').each(function(){
+				var $this = $(this);
+				item[$this.attr('name')] = $this.val();
+			});
+			
+			$loadingGraphic.show();
+			
+			$form.on('mp_cart/after_add_item', function(e, resp){
+				if ( resp.success ) {
+					$.colorbox.close();
+				}
+			});
+			
+			mp_cart.addItem($form, item, qty);
+		}
+	};
+		
+	/**
+	 * Initialization method
+	 *
+	 * @since 3.0
+	 */
 	mp_cart.init = function(){
 		this.initCartAnimation();
 		this.initCartListeners();
 		this.initProductOptionsLightbox();
-	}
+	};
 	
+	/**
+	 * Initialize cart listeners
+	 *
+	 * @since 3.0
+	 */
 	mp_cart.initCartListeners = function(){
 		$('#mp_product_list').on('submit', '.mp_buy_form', function(e){
 			e.preventDefault();
@@ -25,78 +73,68 @@ var mp_cart = {};
 			
 			mp_cart.addItem($this, $this.find('[name="product_id"]').val());
 		});
+		
+		$('#mp_single_product').on('change', '.mp_product_options_att_input_label input', this.updateProductAttributes);
+		$('#mp_single_product').find('.mp_buy_form').validate(this.productAttributeValidationArgs);
 	};
 	
+	/**
+	 * Init colorbox listeners
+	 *
+	 * @since 3.0
+	 * @event cbox_complete
+	 */
 	mp_cart.initCboxListeners = function(){
-		$('.mp_product_options_cb').validate({
-			"errorClass" : "mp_input_error",
-			"errorElement" : "span",
-			"errorPlacement" : function(error, element){
-				error.appendTo(element.closest('.mp_product_options_att').find('.mp_product_options_att_label'));
-			},
-			"ignore" : "",
-			"submitHandler" : function(form){
-				var $form = $(form),
-						$loadingGraphic = $('#cboxLoadingOverlay'),
-						item = {},
-						qty = $form.find('[name="product_quantity"]').val();
-						
-				// Build item object
-				item['product_id'] = $form.find('[name="product_id"]').val();
-				$form.find('[name^="product_attr_"]').filter(':checked').each(function(){
-					var $this = $(this);
-					item[$this.attr('name')] = $this.val();
-				});
-				
-				$loadingGraphic.show();
-				
-				$form.on('mp_cart/after_add_item', function(e, resp){
-					if ( resp.success ) {
-						$.colorbox.close();
-					}
-				});
-				
-				mp_cart.addItem($form, item, qty);
-			}
-		});
-		
-		$('#cboxLoadedContent').on('change', '.mp_product_options_att_input_label input', function(){
-			var $this = $(this),
-					$form = $this.closest('form'),
-					$loadingGraphic = $('#cboxLoadingOverlay'),
-					$qtyChanged = $form.find('input[name="product_qty_changed"]');
-					url = mp_cart_i18n.ajaxurl + '?action=mp_product_update_attributes';
-			
-			$loadingGraphic.show();
-			$this.closest('.mp_product_options_att').nextAll('.mp_product_options_att').find(':radio').prop('checked', false);
-			
-			if ( ! $this.is(':radio') ) {
-				$qtyChanged.val('1');
-			} else {
-				$qtyChanged.val('0');
-			}
-			
-			$.post(url, $form.serialize(), function(resp){
-				$loadingGraphic.hide();
-				
-				if ( resp.success ) {
-					$.each(resp.data, function(index, value){
-						if ( index == 'qty_in_stock' || index == 'out_of_stock' ) {
-							return;
-						}
-						
-						$('#mp_' + index).html(value);
-					});
-					
-					if ( resp.data.out_of_stock ) {
-						alert(resp.data.out_of_stock);
-						$form.find('input[name="product_quantity"]').val(resp.data.qty_in_stock);
-					}
-				}
-			})
-		});
-	}
+		$('.mp_product_options_cb').validate(this.productAttributeValidationArgs);
+		$('#cboxLoadedContent').on('change', '.mp_product_options_att_input_label input', this.updateProductAttributes);
+	};
 	
+	/**
+	 * Update product attributes via ajax
+	 *
+	 * @since 3.0
+	 */
+	mp_cart.updateProductAttributes = function(){
+		var $this = $(this),
+				$form = $this.closest('form'),
+				$loadingGraphic = $('#cboxLoadingOverlay'),
+				$qtyChanged = $form.find('input[name="product_qty_changed"]');
+				url = mp_cart_i18n.ajaxurl + '?action=mp_product_update_attributes';
+		
+		$loadingGraphic.show();
+		$this.closest('.mp_product_options_att').nextAll('.mp_product_options_att').find(':radio').prop('checked', false);
+		
+		if ( ! $this.is(':radio') ) {
+			$qtyChanged.val('1');
+		} else {
+			$qtyChanged.val('0');
+		}
+		
+		$.post(url, $form.serialize(), function(resp){
+			$loadingGraphic.hide();
+			
+			if ( resp.success ) {
+				$.each(resp.data, function(index, value){
+					if ( index == 'qty_in_stock' || index == 'out_of_stock' ) {
+						return;
+					}
+					
+					$('#mp_' + index).html(value);
+				});
+				
+				if ( resp.data.out_of_stock ) {
+					alert(resp.data.out_of_stock);
+					$form.find('input[name="product_quantity"]').val(resp.data.qty_in_stock);
+				}
+			}
+		});
+	};
+	
+	/**
+	 * Initialize product options lightbox for variable products
+	 *
+	 * @since 3.0
+	 */
 	mp_cart.initProductOptionsLightbox = function(){
 		$('.mp_link_buynow').filter('.has_variations').colorbox({
 			"close" : "x",
@@ -105,8 +143,16 @@ var mp_cart = {};
 			},
 			"overlayClose" : false
 		});
-	}
+	};
 	
+	/**
+	 * Add an item to the shopping cart
+	 *
+	 * @since 3.0
+	 * @param object $form The current form object.
+	 * @param int/object item Either an item ID or, if a variable product, an item object.
+	 * @param int qty The quantity to add to the cart. Optional.
+	 */
 	mp_cart.addItem = function( $form, item, qty ){
 		if ( item === undefined || typeof($form) !== 'object' ) {
 			return false;
@@ -159,11 +205,22 @@ var mp_cart = {};
 		});
 	};
 	
+	/**
+	 * Update the cart html
+	 *
+	 * @since 3.0
+	 * @param string html The cart html.
+	 */
 	mp_cart.update = function( html ){
 		$('#mp-floating-cart').replaceWith(html);
 		this.initCartAnimation();
-	}
+	};
 	
+	/**
+	 * Initialize the cart show/hide animation
+	 *
+	 * @since 3.0
+	 */
 	mp_cart.initCartAnimation = function(){
 		var $cart = $('#mp-floating-cart');
 		
