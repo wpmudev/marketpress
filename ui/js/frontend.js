20e03570e67546921964fbcfe585ffbb2879d4ea
Jonathan Cowher <jon@incsub.com>
2014-10-23 11:16:09 -0400

    Update frontend.js
    - Add phpdoc syntax.
    - Add loadingOverlay() method.
    - Remove initToggleVariations() method.

--- ui/js/frontend.js
+++ ui/js/frontend.js
@@ -15,33 +15,63 @@ var marketpress = {};
 }(jQuery));
 
 (function($){
+	/**
+	 * Show or hide the loading overlay
+	 *
+	 * @since 3.0
+	 * @param string action Either show/hide. Optional.
+	 */
+	marketpress.loadingOverlay = function( action ){
+		var $overlay = $('#colorbox').is(':visible') ? $('#cboxLoadingOverlay') : $('#mp-loading-overlay');
+		
+		if ( action === undefined ) {
+			var action = 'show';
+		}
+		
+		if ( $overlay.length == 0 ) {
+			$('body').append('<div id="mp-loading-overlay" style="display:none"></div>');
+			$overlay = $('#mp-loading-overlay');
+		}
+		
+		switch ( action ) {
+			case 'show' :
+				$overlay.show();
+			break;
+			
+			case 'hide' :
+				$overlay.hide();
+			break;
+		}
+	};
+	
+	/**
+	 * Make each product in a product grid row the same height
+	 *
+	 * @since 3.0
+	 */
 	marketpress.equalizeProductGrid = function(){
 		$('.mp_grid_row').each(function(){
 			$(this).find('.mp_one_product').equalHeights().find('.mp_price_buy').addClass('sticky');
 		});
-	}
+	};
 	
+	/**
+	 * Initialize select2 elements
+	 *
+	 * @since 3.0
+	 */
 	marketpress.initSelect2 = function(){
 		$('.mp_select2').select2({
 			"dropdownAutoWidth" : 1,
 			"minimumResultsForSearch" : -1	// hide the search box
 		});
-	}
+	};
 	
-	marketpress.initToggleVariations = function(){
-		$('.mp_variant_image_link').mouseover(function(e){
-			e.preventDefault();
-			
-			var $this = $(this),
-					newSrc = $this.find('.mp_variant_alt_image').find('img').attr('src'),
-					newText = $this.find('.mp_variant_alt_content').html();
-			
-			$this.addClass('selected').siblings().removeClass('selected');
-			$('.mp_product_image_link').attr('href', $this.attr('href')).find('img').attr('src', newSrc);
-			$('.mp_product_content_text').html(newText);
-		});
-	}
-	
+	/**
+	 * Initialize content tabs on the single product template
+	 *
+	 * @since 3.0
+	 */	
 	marketpress.initProductTabs = function(){
 		$('.mp_product_tab_label_link').click(function(e){
 			e.preventDefault();
@@ -53,12 +83,11 @@ var marketpress = {};
 			$tab.addClass('current').siblings().removeClass('current');
 			$target.show().siblings('.mp_product_content').hide();
 		});
-	}
+	};
 }(jQuery));
 
 jQuery(document).ready(function(){
 	marketpress.initSelect2();
-	marketpress.initToggleVariations();
 	marketpress.initProductTabs();
 });
 
