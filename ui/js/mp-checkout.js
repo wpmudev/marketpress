b57dd08403368dc34bf5c581685f7c4bd0aaf556
Jonathan Cowher <jon@incsub.com>
2014-11-21 06:28:17 -0500

    Update mp-checkout.js
    - Add getHash()
    - Add initCheckoutSteps()
    - Add initAjaxLogin()
    - Add initCardValidationListeners()
    - Add initPaymentOptionListeners()

--- ui/js/mp-checkout.js
+++ ui/js/mp-checkout.js
@@ -1,30 +1,205 @@
-var mp_checkout = {};
+var mp_checkout;
+
 (function($){
-	mp_checkout.initListeners = function(){
-		this.initShippingAddressListeners();
-	};
-	
-	mp_checkout.toggleShippingAddressFields = function(){
-		var $cb = $('input[name="enable_shipping_address"]'),
-				$shippingFields = $('[name^="shipping["]');
+	mp_checkout = {
+		/**
+		 * Initialize event listeners
+		 *
+		 * @since 3.0
+		 */
+		initListeners : function(){
+			this.initShippingAddressListeners();
+			this.initPaymentOptionListeners();
+			this.initUpdateShippingMethodsListeners();
+			this.initCardValidationListeners();
+			this.initAjaxLogin();
+			this.initCheckoutSteps();
+		},
 		
-		if ( $cb.prop('checked') ) {
-			$shippingFields.each(function(){
-				$(this).prop('disabled', false);
+		/**
+		 * Get a value from a hashed query string
+		 *
+		 * @since 3.0
+		 * @param string what The name of the variable to retrieve.
+		 * @param mixed defaultVal Optional, what to return if the variable doesn't exist. Defaults to false.
+		 * @return mixed
+		 */
+		getHash : function( what, defaultVal ) {
+			var hash = window.location.hash;
+			
+			if ( undefined === defaultVal ) {
+				defaultVal = false;
+			}
+			
+			if ( 0 > hash.indexOf( '#!' ) || undefined === defaultVal ) {
+				return defaultVal;
+			}
+			
+			var hashParts = hash.substr( 2 ).split( '&' ), hashPairs = {};
+			
+			$.each( hashParts, function( index, value ){
+				var tmp = value.split( '=' );
+				hashPairs[ tmp[0] ] = tmp[1];
 			});
-		} else {
-			$shippingFields.each(function(){
-				$(this).prop('disabled', true);
+			
+			if ( undefined === hashPairs[ what ] ) {
+				return defaultVal;
+			}
+			
+			return hashPairs[ what ];
+		},
+		
+		/**
+		 * Initialize checkout steps
+		 *
+		 * @since 3.0
+		 */
+		initCheckoutSteps : function(){
+			var startingSlide = this.getHash( 'step', 1 );
+			if ( startingSlide <= 0 ) {
+				startingSlide = 1;
+			}
+			
+			$( '#mp-checkout' ).cycle({
+				allowWrap : false,
+				autoHeight : "calc",
+				fx : "scrollHorz",
+				log : false,
+				next : "#mp-checkout .mp-button-checkout-next-step",
+				slideActiveClass : "current",
+				slides : "> .mp-checkout-section",
+				startingSlide : (startingSlide - 1),
+				timeout : 0
 			});
+		},
+		
+		/**
+		 * Initialize AJAX login
+		 *
+		 * @since 3.0
+		 */
+		initAjaxLogin : function(){
+			$( '#mp-checkout' ).submit( function( e ){
+				var $this = $( this ),
+						$email = $this.find( '[name="mp_login_email"]' ),
+						$pass = $this.find( '[name="mp_login_password"]' ),
+						$btn = $( '#mp-button-checkout-login' );
+				
+				if ( $.trim( $email.val() ).length > 0 && $.trim( $pass.val() ).length > 0 ) {
+					e.preventDefault();
+					
+					$btn.ajaxLoading();
+					
+					// Destroy any tooltips
+					if ( $( '#mp-login-tooltip' ).length > 0 ) {
+						$( '#mp-login-tooltip' ).remove();
+					}
+					
+					var data = {
+						action : "mp_ajax_login",
+						email : $email.val(),
+						pass : $pass.val(),
+						mp_login_nonce : $this.find( '[name="mp_login_nonce"]' ).val()
+					};
+					
+					$.post( mp_i18n.ajaxurl, data ).done( function( resp ){
+						if ( resp.success ) {
+							window.location.reload();
+						} else {
+							$btn.ajaxLoading( 'hide' );
+							$email.before( '<a id="mp-login-tooltip"></a>' );
+							$( '#mp-login-tooltip' ).tooltip({
+								items : '#mp-login-tooltip',
+								content : resp.data.message,
+								tooltipClass : "error",
+								open : function( event, ui ){
+									setTimeout( function(){
+										$( '#mp-login-tooltip' ).tooltip( 'destroy' );
+									}, 4000);
+								},
+								position : {
+									of : $email,
+									my : "center bottom-10",
+									at : "center top"
+								},
+								show : 300,
+								hide : 300
+							}).tooltip( 'open' );
+						}
+					});
+				}
+			});
+		},
+		
+		initUpdateShippingMethodsListeners : function(){
+		},
+		
+		/**
+		 * Initialize events related to credit card validation
+		 *
+		 * @since 3.0
+		 */
+		initCardValidationListeners : function(){
+			$( '.mp-input-cc-num' ).payment( 'formatCardNumber' );
+			$( '.mp-input-cc-expiry' ).payment( 'formatCardExpiry' );
+			$( '.mp-input-cc-cvc' ).payment( 'formatCardCVC' )
+		},
+		
+		/**
+		 * Init events related to toggling payment options
+		 *
+		 * @since 3.0
+		 * @access public
+		 */
+		initPaymentOptionListeners : function(){
+			var $input = $( 'input[name="payment_method"]' );
+			
+			$input.change( function(){
+				var $this = $( this ),
+						$target = $( '#mp-gateway-form-' + $this.val() );
+				
+				$target.show().siblings( '.mp_gateway_form' ).hide();
+			});
+			
+			if ( $input.filter( ':checked' ).length ) {
+				$input.filter( ':checked' ).trigger( 'click' );
+			} else {
+				$input.eq(0).trigger( 'click' );
+			}
+		},
+		
+		/**
+		 * Enable/disable shipping address fields
+		 *
+		 * @since 3.0
+		 */
+		toggleShippingAddressFields : function(){
+			var $cb = $( 'input[name="enable_shipping_address"]' ),
+					$shippingFields = $( '[name^="shipping["]' );
+			
+			if ( $cb.prop( 'checked' ) ) {
+				$shippingFields.each( function(){
+					$( this ).prop( 'disabled', false );
+				});
+			} else {
+				$shippingFields.each( function(){
+					$( this ).prop( 'disabled', true );
+				});
+			}
+		},
+	
+		/**
+		 * Initialize events related to enabling/disable shipping address fields
+		 *
+		 * @since 3.0
+		 */
+		initShippingAddressListeners : function(){
+			$( 'input[name="enable_shipping_address"]' ).change( mp_checkout.toggleShippingAddressFields );
 		}
 	};
-	
-	mp_checkout.initShippingAddressListeners = function(){
-		$('input[name="enable_shipping_address"]').change(mp_checkout.toggleShippingAddressFields);
-	};
-}(jQuery));
+}( jQuery ));
 
-jQuery(document).ready(function($){
+jQuery( document ).ready( function( $ ){
 	mp_checkout.initListeners();
 	mp_checkout.toggleShippingAddressFields();
 });
