e45850405e8be0bc610609381ab368ddae4657c5
Jonathan Cowher <jon@incsub.com>
2014-11-21 06:14:09 -0500

    Update mp-coupons.js
    - Use jQuery UI Tooltips instead of proprietary function

--- includes/addons/mp-coupons/ui/js/mp-coupons.js
+++ includes/addons/mp-coupons/ui/js/mp-coupons.js
@@ -25,70 +25,99 @@
 
 var mp_coupons = {};
 (function($){
-	/**
-	 * Remove a coupon
-	 *
-	 * @since 3.0
-	 */
-	mp_coupons.remove = function( id ){
-		var url = mp_coupons_i18n.ajaxurl + '?action=mp_coupons_remove';
-		var data = {
-			"coupon_id" : id	
-		};
-		
-		marketpress.loadingOverlay('show');
-		
-		$.post(url, data).done(function(resp){
-			marketpress.loadingOverlay('hide');
-			marketpress.ajaxEvent('mp_coupons/remove', resp);
+	mp_coupons = {
+		/**
+		 * Remove a coupon
+		 *
+		 * @since 3.0
+		 */
+		remove : function( id ){
+			var url = mp_coupons_i18n.ajaxurl + '?action=mp_coupons_remove';
+			var data = {
+				"coupon_id" : id	
+			};
 			
-			if ( resp.success ) {
-				$.each(resp.data.products, function(key, val){
-					$('#mp-cart-item-' + key).replaceWith(val);
-				});
-				$('#mp-cart-meta').replaceWith(resp.data.cartmeta);
-				marketpress.initSelect2();
-			} else {
-				alert(resp.data.message);
-			}
-		});
-	};
-	
-	/**
-	 * Init coupon form listeners
-	 *
-	 * @since 3.0
-	 */
-	mp_coupons.initCouponFormListeners = function(){
-		var $form = $('#mp-cart-form'),
-				$couponCode = $form.find('[name="mp_cart_coupon"]');
-		
-		$couponCode.on('keyup', function(){
-			$couponCode.mpCartError({
-				"action" : "remove"
+			marketpress.loadingOverlay('show');
+			
+			$.post(url, data).done(function(resp){
+				marketpress.loadingOverlay('hide');
+				marketpress.ajaxEvent('mp_coupons/remove', resp);
+				
+				if ( resp.success ) {
+					$.each(resp.data.products, function(key, val){
+						$('#mp-cart-item-' + key).replaceWith(val);
+					});
+					$('#mp-cart-meta').replaceWith(resp.data.cartmeta);
+					marketpress.initSelect2();
+				} else {
+					alert(resp.data.message);
+				}
 			});
-		});
-	
-		$form.submit(function(e){
-			var couponCode = $couponCode.val().toUpperCase().replace(/[^A-Z0-9]/g, '');
+		},
+		
+		/**
+		 * Init coupon form listeners
+		 *
+		 * @since 3.0
+		 */
+		initCouponFormListeners : function(){
+			var $form = $('#mp-cart-form'),
+					$couponCode = $form.find('[name="mp_cart_coupon"]');
 			
-			if ( couponCode.length > 0 ) {
+			// Create tooltip for future use
+			$couponCode.before( '<div id="mp-coupon-tooltip" />' );
+			$( '#mp-coupon-tooltip' ).tooltip({
+				items : "#mp-coupon-tooltip",
+				tooltipClass : "error",
+				content : "",
+				position : {
+					of : $couponCode,
+					my : "center bottom-10",
+					at : "center top"
+				},
+				show : 400,
+				hide : 400
+			});
+			var $tooltip = $( '#mp-coupon-tooltip' );
+			
+			$form.submit(function(e){
 				e.preventDefault();
-				mp_cart.applyCoupon(couponCode, $form);
-			}
-		})
-		.on('mp_cart/apply_coupon/error', function(e, message){
-			$couponCode.mpCartError({
-				"action" : "add",
-				"message" : message
-			});
-		})
-		.on('mp_cart/apply_coupon/success', function(e, data){
-			$.each(data.products, function(index, value){
-				$('#mp-cart-item-' + index).replaceWith(value);
-			});
-			$('#mp-cart-meta').replaceWith(data.cart_meta);
-		});		
+				var couponCode = $couponCode.val().toUpperCase().replace(/[^A-Z0-9]/g, '');
+				
+				if ( couponCode.length > 0 ) {
+					mp_cart.applyCoupon( couponCode, $form );
+				} else {
+					$tooltip
+						.tooltip( 'option', 'content', mp_coupons_i18n.messages.required )
+						.tooltip( 'option', 'tooltipClass', 'error' )
+						.tooltip( 'open' );
+				}
+			})
+			.on('mp_cart/apply_coupon/error', function( e, message ){
+				$tooltip
+					.tooltip( 'option', 'content', message )
+					.tooltip( 'option', 'tooltipClass', 'error' )
+					.tooltip( 'open' );
+			})
+			.on('mp_cart/apply_coupon/success', function( e, data ){
+				$couponCode.val( '' );
+								
+				$.each( data.products, function( index, value ){
+					$( '#mp-cart-item-' + index ).replaceWith( value );
+				});
+				$( '#mp-cart-meta').replaceWith( data.cart_meta );
+				$tooltip
+					.tooltip( 'option', 'content', mp_coupons_i18n.messages.added )
+					.tooltip( 'option', 'tooltipClass', 'success' )
+					.tooltip( 'option', 'open', function(){
+						setTimeout( function(){
+							$tooltip.tooltip( 'close' );
+							$tooltip.tooltip( 'option', 'open', function(){} );
+						}, 4000);
+					})
+					.tooltip( 'open' );
+			});		
+		}
 	};
 }(jQuery));
 
