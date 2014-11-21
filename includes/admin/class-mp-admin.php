ffec184760e070fad085c8686cc59960c2a6f9d2
Jonathan Cowher <jon@incsub.com>
2014-11-21 06:14:41 -0500

    Code formatting

--- includes/admin/class-mp-admin.php
+++ includes/admin/class-mp-admin.php
@@ -18,7 +18,7 @@ class MP_Admin {
 	 * @return object
 	 */
 	public static function get_instance() {
-		if ( is_null(self::$_instance) ) {
+		if ( is_null( self::$_instance ) ) {
 			self::$_instance = new MP_Admin();
 		}
 		return self::$_instance;
@@ -31,77 +31,62 @@ class MP_Admin {
 	 * @access private
 	 */
 	private function __construct() {
-		$this->includes();
+		$this->_init_dash_notices();
+		$this->_includes();
 		
 		//save orders screen options
-		add_filter('set-screen-option', array(&$this, 'save_orders_screen_options'), 10, 3);
+		add_filter( 'set-screen-option', array( &$this, 'save_orders_screen_options' ), 10, 3 );
 		//set custom post-updated messages
-		add_filter('post_updated_messages', array(&$this, 'post_updated_messages'));
+		add_filter( 'post_updated_messages', array( &$this, 'post_updated_messages' ) );
 		//enqueue styles and scripts
-		add_action('admin_enqueue_scripts', array(&$this, 'enqueue_styles_scripts'));
+		add_action( 'admin_enqueue_scripts', array( &$this, 'enqueue_styles_scripts' ) );
 	}
-	
-	/**
-	 * Enqueue styles and scripts
-	 *
-	 * @since 3.0
-	 * @access public
-	 */
-	public function enqueue_styles_scripts() {
-		wp_enqueue_style('mp-admin', mp_plugin_url('includes/admin/ui/css/admin.css'), array(), MP_VERSION);
-	}
-	
+
 	/**
 	 * Includes any necessary files
 	 *
 	 * @since 3.0
 	 * @access public
 	 */
-	public function includes() {
-		require_once mp_plugin_dir('includes/admin/class-mp-orders-admin.php');
-		require_once mp_plugin_dir('includes/admin/class-mp-products-admin.php');
-		require_once mp_plugin_dir('includes/admin/class-mp-product-attributes-admin.php');
-		require_once mp_plugin_dir('includes/admin/class-mp-store-settings-admin.php');
-		require_once mp_plugin_dir('includes/admin/class-mp-setup-wizard.php');
-		require_once mp_plugin_dir('includes/admin/class-mp-shortcode-builder.php');
+	protected function _includes() {
+		require_once mp_plugin_dir( 'includes/admin/class-mp-orders-admin.php' );
+		require_once mp_plugin_dir( 'includes/admin/class-mp-products-admin.php' );
+		require_once mp_plugin_dir( 'includes/admin/class-mp-product-attributes-admin.php' );
+		require_once mp_plugin_dir( 'includes/admin/class-mp-store-settings-admin.php' );
+		require_once mp_plugin_dir( 'includes/admin/class-mp-setup-wizard.php' );
+		require_once mp_plugin_dir( 'includes/admin/class-mp-shortcode-builder.php' );
 	}
-	
+
 	/**
-	 * Modifies the post-updated messages for the mp_order, product and mp_coupon post types
+	 * Initialize dash notices
 	 *
 	 * @since 3.0
 	 * @access public
-	 * @filter post_updated_messages
-	 * @param array $messages
-	 * @return array
 	 */
-	public function post_updated_messages( $messages ) {
-		global $post, $post_ID;
+	protected function _init_dash_notices() {
+		if ( MP_LITE ) {
+			return;
+		}
 		
-		$post_type = get_post_type($post_ID);
-		
-		if ( $post_type != 'mp_order' && $post_type != MP_Product::get_post_type() && $post_type != 'mp_coupon' ) { return $messages; }
-		
-		$obj = get_post_type_object($post_type);
-		$singular = $obj->labels->singular_name;
-		
-		$messages[$post_type] = array(
-			0 => '', // Unused. Messages start at index 1.
-			1 => sprintf(__($singular.' updated. <a href="%s">View ' . strtolower($singular) . '</a>'), esc_url(get_permalink($post_ID))),
-			2 => __('Custom field updated.'),
-			3 => __('Custom field deleted.'),
-			4 => __($singular.' updated.'),
-			5 => isset($_GET['revision']) ? sprintf(__($singular . ' restored to revision from %s'), wp_post_revision_title((int) $_GET['revision'], false)) : false,
-			6 => sprintf(__($singular.' published. <a href="%s">View ' . strtolower($singular).'</a>'), esc_url(get_permalink($post_ID))),
-			7 => __('Page saved.'),
-			8 => sprintf(__($singular . ' submitted. <a target="_blank" href="%s">Preview ' . strtolower($singular).'</a>'), esc_url(add_query_arg('preview', 'true', get_permalink($post_ID)))),
-			9 => sprintf(__($singular . ' scheduled for: <strong>%1$s</strong>. <a target="_blank" href="%2$s">Preview ' . strtolower($singular).'</a>'), date_i18n(__('M j, Y @ G:i'), strtotime($post->post_date)), esc_url(get_permalink($post_ID))),
-			10 => sprintf(__($singular . ' draft updated. <a target="_blank" href="%s">Preview ' . strtolower($singular).'</a>'), esc_url(add_query_arg( 'preview', 'true', get_permalink($post_ID)))),
+		//! TODO: update screen ids for dash notices
+		global $wpmudev_notices;
+		$wpmudev_notices[] = array(
+			'id'=> 144,
+			'name'=> 'MarketPress',
+			'screens' => array(
+				'edit-product',
+				'edit-mp_product',
+				'product',
+				'mp_product',
+				'edit-product_category',
+				'edit-product_tag',
+				'settings_page_marketpress-ms-network'
+			)
 		);
 		
-		return $messages;
+		require_once mp_plugin_dir( 'includes/admin/dash-notice/wpmudev-dash-notification.php' );
 	}
-							
+
 	/**
 	 * Adds the MarketPress help tab
 	 *
@@ -111,7 +96,7 @@ class MP_Admin {
 	public function add_help_tab() {
 		get_current_screen()->add_help_tab(array(
 			'id' => 'marketpress-help',
-			'title' => __('MarketPress Instructions', 'mp'),
+			'title' => __( 'MarketPress Instructions', 'mp' ),
 			'content' => '<iframe src="//premium.wpmudev.org/wdp-un.php?action=help&id=144" width="100%" height="600px"></iframe>'
 		));
 	}
@@ -183,6 +168,54 @@ class MP_Admin {
 		</div>
 		<?php	
 	}
+	
+	/**
+	 * Enqueue styles and scripts
+	 *
+	 * @since 3.0
+	 * @access public
+	 */
+	public function enqueue_styles_scripts() {
+		wp_enqueue_style( 'mp-admin', mp_plugin_url( 'includes/admin/ui/css/admin.css' ), array(), MP_VERSION );
+	}
+			
+	/**
+	 * Modifies the post-updated messages for the mp_order, product and mp_coupon post types
+	 *
+	 * @since 3.0
+	 * @access public
+	 * @filter post_updated_messages
+	 * @param array $messages
+	 * @return array
+	 */
+	public function post_updated_messages( $messages ) {
+		global $post, $post_ID;
+		
+		$post_type = get_post_type( $post_ID );
+		
+		if ( $post_type != 'mp_order' && $post_type != MP_Product::get_post_type() && $post_type != 'mp_coupon' ) {
+			return $messages;
+		}
+		
+		$obj = get_post_type_object($post_type);
+		$singular = $obj->labels->singular_name;
+		
+		$messages[ $post_type ] = array(
+			0 => '', // Unused. Messages start at index 1.
+			1 => sprintf(__($singular.' updated. <a href="%s">View ' . strtolower($singular) . '</a>'), esc_url(get_permalink($post_ID))),
+			2 => __('Custom field updated.'),
+			3 => __('Custom field deleted.'),
+			4 => __($singular.' updated.'),
+			5 => isset($_GET['revision']) ? sprintf(__($singular . ' restored to revision from %s'), wp_post_revision_title((int) $_GET['revision'], false)) : false,
+			6 => sprintf(__($singular.' published. <a href="%s">View ' . strtolower($singular).'</a>'), esc_url(get_permalink($post_ID))),
+			7 => __('Page saved.'),
+			8 => sprintf(__($singular . ' submitted. <a target="_blank" href="%s">Preview ' . strtolower($singular).'</a>'), esc_url(add_query_arg('preview', 'true', get_permalink($post_ID)))),
+			9 => sprintf(__($singular . ' scheduled for: <strong>%1$s</strong>. <a target="_blank" href="%2$s">Preview ' . strtolower($singular).'</a>'), date_i18n(__('M j, Y @ G:i'), strtotime($post->post_date)), esc_url(get_permalink($post_ID))),
+			10 => sprintf(__($singular . ' draft updated. <a target="_blank" href="%s">Preview ' . strtolower($singular).'</a>'), esc_url(add_query_arg( 'preview', 'true', get_permalink($post_ID)))),
+		);
+		
+		return $messages;
+	}							
 }
 
 MP_Admin::get_instance();
