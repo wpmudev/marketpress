d8ba637af9ee8b58e5fbc30cd4c03e6a02cb2090
Jonathan Cowher <jon@incsub.com>
2014-12-07 21:02:42 -0500

    Add lookup_order() method

--- includes/admin/class-mp-ajax.php
+++ includes/admin/class-mp-ajax.php
@@ -25,80 +25,11 @@ class MP_Ajax {
 	}
 	
 	/**
-	 * Creates a store page and sets it to a draft state
+	 * 
 	 *
 	 * @since 3.0
 	 * @access public
 	 */
-	public function create_store_page() {
-		check_admin_referer('mp_create_store_page');
-		
-		$type = mp_get_get_value('type');
-		$args = array();
-		$defaults = array(
-			'post_status' => 'publish',
-			'post_type' => 'page',
-		);
-		
-		switch ( $type ) {
-			case 'store' :
-				$args = array(
-					'post_title' => __('Store', 'mp'),
-					'post_content' => __("Welcome to our online store! Feel free to browse around:\n\n[mp_store_navigation]\n\nCheck out our most popular products:\n\n[mp_popular_products]\n\nBrowse by category:\n\n[mp_list_categories]\n\nBrowse by tag:\n\n[mp_tag_cloud]", 'mp'),
-				);
-			break;
-			
-			case 'network_store_page' :
-				$args = array(
-					'post_title' => __('Global Store', 'mp'),
-					'post_content' => __("Welcome to our market place!\n\nCheck out our network of products:\n\n[mp_list_global_products]\n\nBrowse by category:\n\n[mp_global_categories_list]\n\nBrowse by tag:\n\n[mp_global_tag_cloud]", 'mp'),
-				);
-			break;
-			
-			case 'products' :
-				$args = array(
-					'post_title' => __('Products', 'mp'),
-					'post_content' => '[mp_list_products]',
-					'post_parent' => mp_get_setting('pages->store', 0),
-				);
-			break;
-			
-			case 'cart' :
-				$args = array(
-					'post_title' => __('Cart', 'mp'),
-					'post_content' => '[mp_cart]',
-					'post_parent' => mp_get_setting('pages->store', 0)
-				);
-			break;
-			
-			case 'checkout' :
-				$args = array(
-					'post_title' => __('Checkout', 'mp'),
-					'post_content' => '[mp_cart]',
-					'post_parent' => mp_get_setting('pages->store', 0)
-				);
-			break;
-			
-			case 'order_status' :
-				$args = array(
-					'post_title' => __('Order Status', 'mp'),
-					'post_content' => '[mp_order_status]',
-					'post_parent' => mp_get_setting('pages->store', 0)
-				);
-			break;
-		}
-		
-		$post_id = wp_insert_post(array_merge($defaults, $args));
-		MP_Pages_Admin::get_instance()->save_store_page_value($type, $post_id, false);
-		wp_send_json_success(array(
-			'post_id' => $post_id,
-			'select2_value' => $post_id . '->' . get_the_title($post_id),
-			'button_html' => '<a target="_blank" class="button mp-edit-page-button" href="' . add_query_arg(array(
-				'post' => $post_id,
-				'action' => 'edit',
-			), get_admin_url(null, 'post.php')) . '">' . __('Edit Page', 'mp') . '</a>',
-		));
-	}
 	
 	/**
 	 * Process ajax login
@@ -153,6 +84,51 @@ class MP_Ajax {
 		
 		die;
 	}
+
+	/**
+	 * Create a store page
+	 *
+	 * @since 3.0
+	 * @access public
+	 * @action wp_ajax_mp_create_store_page
+	 */
+	public function create_store_page() {
+		check_admin_referer( 'mp_create_store_page' );
+		
+		$type = mp_get_get_value( 'type' );
+		$post_id = mp_create_store_page( $type );
+		
+		wp_send_json_success( array(
+			'post_id' => $post_id,
+			'select2_value' => $post_id . '->' . get_the_title( $post_id ),
+			'button_html' => '<a target="_blank" class="button mp-edit-page-button" href="' . add_query_arg( array(
+				'post' => $post_id,
+				'action' => 'edit',
+			), get_admin_url( null, 'post.php' ) ) . '">' . __( 'Edit Page', 'mp' ) . '</a>',
+		) );
+	}
+	
+	/**
+	 * Look up an order by it's ID
+	 *
+	 * @since 3.0
+	 * @access public
+	 * @action wp_ajax_mp_lookup_order, wp_ajax_nopriv_mp_lookup_order
+	 */
+	public function lookup_order( ) {
+		if ( $order_id = mp_get_post_value( 'order_id' ) ) {
+			$order = new MP_Order( $order_id );
+			if ( $order->exists() ) {
+				wp_send_json_success( array(
+					'redirect_url' => trailingslashit( mp_store_page_url( 'order_status', false ) ) . $order->get_id(),
+				) );
+			}
+		}
+		
+		wp_send_json_error( array(
+			'error_message' => __( 'Oops... we could not locate any orders by that ID. Please double check your order ID and try again.', 'mp' ),
+		) );
+	}
 	
 	/**
 	 * Constructor function
@@ -161,17 +137,20 @@ class MP_Ajax {
 	 * @access private
 	 */
 	private function __construct() {
-		add_action('wp_ajax_mp_create_store_page', array(&$this, 'create_store_page'));
-		add_action('wp_ajax_mp_bulk_edit_products', array(&$this, 'bulk_edit_products'));
-		add_action('wp_ajax_mp_change_order_status', array('MP_Orders_Admin', 'ajax_change_order_status'));
+		add_action( 'wp_ajax_mp_create_store_page', array( &$this, 'create_store_page' ) );
+		add_action( 'wp_ajax_mp_bulk_edit_products', array( &$this, 'bulk_edit_products' ) );
+		add_action( 'wp_ajax_mp_change_order_status', array( 'MP_Orders_Admin', 'ajax_change_order_status' ) );
 		// Get product variation colorbox
-		add_action('wp_ajax_mp_product_get_variations_lightbox', array('MP_Product', 'ajax_display_variations_lightbox'));
-		add_action('wp_ajax_nopriv_mp_product_get_variations_lightbox', array('MP_Product', 'ajax_display_variations_lightbox'));
+		add_action( 'wp_ajax_mp_product_get_variations_lightbox', array( 'MP_Product', 'ajax_display_variations_lightbox' ) );
+		add_action( 'wp_ajax_nopriv_mp_product_get_variations_lightbox', array( 'MP_Product', 'ajax_display_variations_lightbox' ) );
 		// Update product attributes
-		add_action('wp_ajax_mp_product_update_attributes', array('MP_Product', 'ajax_update_attributes'));
-		add_action('wp_ajax_nopriv_mp_product_update_attributes', array('MP_Product', 'ajax_update_attributes'));
+		add_action( 'wp_ajax_mp_product_update_attributes', array( 'MP_Product', 'ajax_update_attributes' ) );
+		add_action( 'wp_ajax_nopriv_mp_product_update_attributes', array( 'MP_Product', 'ajax_update_attributes' ) );
 		// Ajax login
 		add_action( 'wp_ajax_nopriv_mp_ajax_login', array( &$this, 'ajax_login' ) );
+		// Look up order
+		add_action( 'wp_ajax_mp_lookup_order', array( &$this, 'lookup_order' ) );
+		add_action( 'wp_ajax_nopriv_mp_lookup_order', array( &$this, 'lookup_order' ) );
 	}
 }
 
