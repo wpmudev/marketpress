93afc62c54a01f3350f846c3b772cd2e2d07c69a
Jonathan Cowher <jon@incsub.com>
2014-12-21 16:00:11 -0500

    Remove South Africa as country that doesn't use zip codes

--- marketpress-includes/marketpress-data.php
+++ marketpress-includes/marketpress-data.php
@@ -304,7 +304,6 @@ $this->countries_no_postcode = array(
   "SL" => "Sierra Leone",
   "SB" => "Solomon Islands",
   "SO" => "Somalia",
-  "ZA" => "South Africa",
   "SR" => "Suriname",
   "SY" => "Syrian Arab Republic",
   "TZ" => "Tanzania, United Republic Of",
