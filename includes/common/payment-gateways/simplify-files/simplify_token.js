d847cb057841126b57f25503c1d0713b924cb0a7
Jonathan Cowher <jon@incsub.com>
2015-01-19 17:38:44 -0500

    Update Simplify gateway for 3.0 checkout

--- includes/common/payment-gateways/simplify-files/simplify_token.js
+++ includes/common/payment-gateways/simplify-files/simplify_token.js
@@ -1,81 +1,95 @@
-/*
- * Copyright (c) 2013, MasterCard International Incorporated
- * All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without modification, are 
- * permitted provided that the following conditions are met:
- * 
- * Redistributions of source code must retain the above copyright notice, this list of 
- * conditions and the following disclaimer.
- * Redistributions in binary form must reproduce the above copyright notice, this list of 
- * conditions and the following disclaimer in the documentation and/or other materials 
- * provided with the distribution.
- * Neither the name of the MasterCard International Incorporated nor the names of its 
- * contributors may be used to endorse or promote products derived from this software 
- * without specific prior written permission.
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES 
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT 
- * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
- * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER 
- * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING 
- * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF 
- * SUCH DAMAGE.
- */
-(function($){
+( function( $ ) {
+	$( document ).on( 'mp_checkout_process_simplify', generateToken );
 
-	function simplifyResponseHandler(data) {
-		$(".error").remove();
-		if(data.error) {
-			if(data.error.code == "validation") {
-				var fieldErrors = data.error.fieldErrors,
-						fieldErrorsLength = fieldErrors.length;
-				$('#cc-cvc, #cc-number, #cc-exp-month, #cc-exp-year').css('box-shadow', 'none');
-				for (var i = 0; i < fieldErrorsLength; i++) {
-					if(fieldErrors[i].field == 'card.cvc') {
-						$('#cc-cvc').css('box-shadow', '0px 0px 5px red');
-					} else if(fieldErrors[i].field == 'card.number') {
-						$('#cc-number').css('box-shadow', '0px 0px 5px red');
-					} else if(fieldErrors[i].field == 'card.expMonth') {
-						$('#cc-exp-month').css('box-shadow', '0px 0px 5px red');
-					} else if(fieldErrors[i].field == 'card.expYear') {
-						$('#cc-exp-year').css('box-shadow', '0px 0px 5px red');
-					}
-				}
+	$( document ).on( 'mp_checkout/step_changed', function( evt, $out, $in ) {
+		if ( $in.next( '.mp-checkout-section' ).length == 0 ) {
+			addInputNames();
+		}
+	} );	
+
+	/**
+	 * Add input names for jQuery validation plugin
+	 *
+	 * Not ideal, but the jQuery validate plugin requires fields to have names
+	 * so this function will add them at the last possible minute
+	 *
+	 * @since 3.0
+	 */
+	function addInputNames() {
+		$( '#mp-cc-num' ).attr( 'name', 'mp_cc_num' );
+		$( '#mp-cc-exp' ).attr( 'name', 'mp_cc_exp' );
+		$( '#mp-cc-cvc' ).attr( 'name', 'mp_cc_cvc' );
+	}
+
+	/**
+	 * Remove CC input names before submitting form for PCI compliance
+	 *
+	 * @since 3.0
+	 */
+	function removeInputNames() {
+		$( '#mp-cc-num, #mp-cc-exp, #mp-cc-cvc' ).removeAttr( 'name' );
+	}
+
+	/**
+	 * Generate card token
+	 *
+	 * @since 3.0
+	 */		
+	function generateToken() {
+		marketpress.loadingOverlay( 'show' );
+		removeInputNames();
+		
+		var expObj = $.payment.cardExpiryVal( $( '#mp-cc-exp' ).val() );
+		SimplifyCommerce.generateToken( {
+			key : simplify.publicKey,
+			card : {
+				number : $( "#mp-cc-num" ).val().replace( /[^0-9]/ig, '' ),
+				cvc : $( "#mp-cc-cvc" ).val(),
+				expMonth : expObj.month,
+				expYear : expObj.year.toString().substr( 2, 2 ),
+				name : $( '#mp-cc-name' ).val().substr( 0, 50 ),
+				addressLine1 : $( '[name="billing[address1]"]' ).val().substr( 0, 255 ),
+				addressLine2 : ( $( '[name="billing[address2]"]' ).length > 0 ) ? $( '[name="billing[address2]"]' ).val().substr( 0, 255 ) : '',
+				addressCity : $( '[name="billing[city]"]' ).val().substr( 0, 50 ),
+				addressState : $( '[name="billing[state]"]' ).val().substr( 0, 2 ),
+				addressZip : $( '[name="billing[zip]"]' ).val().substr( 0, 9 ),
+				addressCountry : $( '[name="billing[country]"]' ).val().substr( 0, 2 )
 			}
-			$("#mp_payment_confirm").removeAttr("disabled");
+		}, responseHandler );
+	}
+
+  /**
+	 * Show/hide the payment error message
+	 *
+	 * @since 3.0
+	 * @param string action Either "show" or "hide".
+	 * @param string message The message to show. Required if action is "show".
+	 */
+	function errorMessage( action, message ) {
+		var $errors = $( '#mp-checkout-payment-form-errors' );
+		
+		if ( 'show' == action ) {
+			$errors.html( message ).addClass( 'show' );
 		} else {
-			var token = data["id"];
-			$("#mp_payment_form").append("<input type='hidden' name='simplifyToken' value='" + token + "' />");
-			$("#mp_payment_form").get(0).submit();
+			$errors.removeClass( 'show' );
 		}
 	}
-	
-	$(document).ready(function() {
-		$("#mp_payment_form").on("submit", function(event) {
-			var $this = $(this),
-					$gateway = $this.find('.mp_choose_gateway');
+
+	function responseHandler( data, status ) {
+		if ( status != 200 ) {
+			marketpress.loadingOverlay( 'hide' );
+			errorMessage( 'show', data.error.message );
+		} else {
+			var $form = $( '#mp-checkout' );
+			var $paymentForm = $( '#mp-gateway-form-simplify' );
 			
-			if ( $gateway.length && $gateway.filter(':checked').val() != 'simplify' )
-				//simplify gateway is not checked so let's bail to prevent conflicts with other gateways
-				return;
+			$( '<input/>' )
+				.attr( { type : "hidden", name : "simplify_token" } )
+				.val( data.id )
+				.appendTo( $paymentForm );
 			
-			event.preventDefault();
-			
-			$("#mp_payment_confirm").attr("disabled", "disabled");
-			
-			SimplifyCommerce.generateToken({
-				"key" : simplify.publicKey,
-				"card" : {
-					"number" : $("#cc-number").val(),
-					"cvc" : $("#cc-cvc").val(),
-					"expMonth" : $("#cc-exp-month").val(),
-					"expYear" : $("#cc-exp-year").val()
-				}
-			}, simplifyResponseHandler);
-		});
-	});
+			$form.get( 0 ).submit();
+		}
+	}
 	
 }(jQuery));
