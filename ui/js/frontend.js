7edb747919e49913b4b39b83845a3ce2323d7cd3
Marko Miljus <marko@incsub.com>
2015-05-29 13:17:49 +0200

    Adding equalHeights on resize

--- ui/js/frontend.js
+++ ui/js/frontend.js
@@ -1,17 +1,14 @@
 ( function( $ ) {
-    $.fn.equalHeights = function() {
+    $.fn.equalHeights = function( ) {
         var maxHeight = 0;
-
-        this.each( function() {
-            maxHeight = Math.max( $( this ).height(), maxHeight );
+        this.each( function( ) {
+            maxHeight = Math.max( $( this ).height( ), maxHeight );
         } );
-
-        return this.each( function() {
+        return this.each( function( ) {
             $( this ).height( maxHeight );
         } );
     }
 }( jQuery ) );
-
 ( function( $ ) {
     /**
      * Preload loading icon
@@ -19,7 +16,6 @@
      * @since 3.0
      */
     $( '<img />' ).get( 0 ).src = mp_i18n.loadingImage;
-
     /**
      * Add or remove cart ajax loading icon
      *
@@ -31,18 +27,28 @@
             var action = 'show';
         }
 
-        return this.each( function() {
+        return this.each( function( ) {
             if ( 'show' == action ) {
-                $( this ).hide().after( '<img src="' + mp_i18n.loadingImage + '" alt="" />' );
+                $( this ).hide( ).after( '<img src="' + mp_i18n.loadingImage + '" alt="" />' );
             } else {
-                $( this ).show().next( 'img' ).remove();
+                $( this ).show( ).next( 'img' ).remove( );
             }
         } );
     };
 }( jQuery ) );
-
 var marketpress = { };
 ( function( $ ) {
+
+    $( window ).resize( function( ) {
+        $( '.mp_grid_row' ).each( function( ) {
+            var $this = $( this );
+            $this.find( '.mp_product_detail' ).equalHeights( );
+            $this.find( '.mp_one_product' ).equalHeights( );
+            $this.find( '.mp_buy_form' ).addClass( 'sticky' );
+        } );
+    } );
+
+
     marketpress = {
         /**
          * Show or hide the loading overlay
@@ -52,7 +58,6 @@ var marketpress = { };
          */
         loadingOverlay: function( action ) {
             var $overlay = $( '#colorbox' ).is( ':visible' ) ? $( '#cboxLoadingOverlay' ) : $( '#mp-loading-overlay' );
-
             if ( action === undefined ) {
                 var action = 'show';
             }
@@ -64,11 +69,10 @@ var marketpress = { };
 
             switch ( action ) {
                 case 'show' :
-                    $overlay.show();
+                    $overlay.show( );
                     break;
-
                 case 'hide' :
-                    $overlay.hide();
+                    $overlay.hide( );
                     break;
             }
         },
@@ -77,17 +81,16 @@ var marketpress = { };
          *
          * @since 3.0
          */
-        initToolTips: function() {
+        initToolTips: function( ) {
             $( document ).tooltip( {
                 "items": ".mp-tooltip-help, .mp-has-tooltip",
-                "content": function() {
+                "content": function( ) {
                     var $this = $( this );
-
                     if ( $this.is( '.mp-has-tooltip' ) ) {
                         $this = $this.next( '.mp-tooltip-content' );
                     }
 
-                    return $this.html();
+                    return $this.html( );
                 },
                 "position": {
                     "my": "center bottom-10",
@@ -102,12 +105,11 @@ var marketpress = { };
          *
          * @since 3.0
          */
-        equalizeProductGrid: function() {
-            $( '.mp_grid_row' ).each( function() {
+        equalizeProductGrid: function( ) {
+            $( '.mp_grid_row' ).each( function( ) {
                 var $this = $( this );
-
-                $this.find( '.mp_product_detail' ).equalHeights();
-                $this.find( '.mp_one_product' ).equalHeights();
+                $this.find( '.mp_product_detail' ).equalHeights( );
+                $this.find( '.mp_one_product' ).equalHeights( );
                 $this.find( '.mp_buy_form' ).addClass( 'sticky' );
             } );
         },
@@ -116,12 +118,11 @@ var marketpress = { };
          *
          * @since 3.0
          */
-        initSelect2: function() {
+        initSelect2: function( ) {
             $( 'select.mp_select2' ).not( '.select2-offscreen' ).select2( {
                 "dropdownAutoWidth": 1,
                 "minimumResultsForSearch": -1	// hide the search box
             } );
-
             $( 'select.mp_select2_search' ).not( '.select2-offscreen' ).select2( {
                 "dropdownAutoWidth": 1
             } );
@@ -131,33 +132,27 @@ var marketpress = { };
          *
          * @since 3.0
          */
-        initOrderLookup: function() {
+        initOrderLookup: function( ) {
             var $form = $( '#mp-order-lookup-form' );
             var $btn = $form.find( '[type="submit"]' );
             var $input = $form.find( 'input' );
-
             $form.on( 'submit', function( e ) {
-                e.preventDefault();
-
+                e.preventDefault( );
                 if ( $btn.is( ':hidden' ) ) {
                     // Already searching for an order - bail
                     return;
                 }
 
-                var data = $form.serialize();
-
+                var data = $form.serialize( );
                 $btn.ajaxLoading( 'show' );
                 $input.prop( 'disabled', true );
-
                 $.post( $form.attr( 'action' ), data ).done( function( resp ) {
                     if ( resp.success ) {
                         window.location.href = resp.data.redirect_url;
                     } else {
                         var $tooltip = $input.prev( '.mp-tooltip' );
-
                         $btn.ajaxLoading( 'hide' );
                         $input.prop( 'disabled', false );
-
                         if ( $tooltip.length == 0 ) {
                             $input.before( '<div class="mp-tooltip"></div>' );
                             $tooltip = $input.prev( '.mp-tooltip' );
@@ -185,27 +180,23 @@ var marketpress = { };
          *
          * @since 3.0
          */
-        initProductTabs: function() {
+        initProductTabs: function( ) {
 
             $( '.mp_product_meta a.more-link' ).live( 'click', function( e ) {
-                e.preventDefault();
-                $( '#mp_single_product a.mp_product_tab_label_link.mp-product-overview' ).click();
+                e.preventDefault( );
+                $( '#mp_single_product a.mp_product_tab_label_link.mp-product-overview' ).click( );
                 $( 'html, body' ).animate( {
-                    scrollTop: $( "a.mp_product_tab_label_link.mp-product-overview" ).offset().top - 30
+                    scrollTop: $( "a.mp_product_tab_label_link.mp-product-overview" ).offset( ).top - 30
                 }, 500 );
             } );
-
             $( '.mp_product_tab_label_link' ).click( function( e ) {
-                e.preventDefault();
-
+                e.preventDefault( );
                 var $this = $( this ),
-                    $tab = $this.parent(),
+                    $tab = $this.parent( ),
                     $target = $( $this.attr( 'href' ) );
-
-                $tab.addClass( 'current' ).siblings().removeClass( 'current' );
-                $target.show().siblings( '.mp_product_content' ).hide();
+                $tab.addClass( 'current' ).siblings( ).removeClass( 'current' );
+                $target.show( ).siblings( '.mp_product_content' ).hide( );
             } );
-
             if ( window.location.hash.length > 0 ) {
                 var $target = $( '[href="' + window.location.hash + '"]' );
                 if ( $target.length > 0 ) {
@@ -228,7 +219,6 @@ var marketpress = { };
 
             var successEvent = event + '/success';
             var errorEvent = event + '/error';
-
             /**
              * Fires whether the response was successful or not
              *
@@ -236,7 +226,6 @@ var marketpress = { };
              * @param object The response data.
              */
             $scope.trigger( event, [ resp ] );
-
             if ( resp.success ) {
                 /**
                  * Fires on success
@@ -247,7 +236,6 @@ var marketpress = { };
                 $scope.trigger( successEvent.replace( '//', '/' ), [ resp.data ] );
             } else {
                 var message = ( resp.data === undefined ) ? '' : resp.data.message;
-
                 /**
                  * Fires on error
                  *
@@ -262,7 +250,7 @@ var marketpress = { };
          *
          * @since 3.0
          */
-        initImageLightbox: function() {
+        initImageLightbox: function( ) {
             $( '.mp_product_image_link' ).filter( '.mp_lightbox' ).colorbox( {
                 maxWidth: "90%",
                 maxHeight: "90%",
@@ -274,18 +262,14 @@ var marketpress = { };
          *
          * @since 3.0
          */
-        initProductFiltersPagination: function() {
+        initProductFiltersPagination: function( ) {
             var $form = $( '#mp_product_list_refine' );
-
             $form.on( 'change', 'select', function( e ) {
                 var $this = $( this );
-
                 // Redirect if product category dropdown changed
                 if ( 'product_category' == $this.attr( 'name' ) ) {
-                    var val = $this.val();
-
+                    var val = $this.val( );
                     marketpress.loadingOverlay( 'show' );
-
                     if ( typeof ( mp_i18n.productCats[ val ] ) == 'undefined' ) {
                         window.location.href = mp_i18n.productsURL;
                     } else {
@@ -294,19 +278,16 @@ var marketpress = { };
 
                     return;
                 } else {
-                    marketpress.updateProductList();
+                    marketpress.updateProductList( );
                 }
             } );
-
-            $( '#mp_product_nav' ).parent().on( 'click', '#mp_product_nav a', function( e ) {
-                e.preventDefault();
-
+            $( '#mp_product_nav' ).parent( ).on( 'click', '#mp_product_nav a', function( e ) {
+                e.preventDefault( );
                 var $this = $( this );
                 var href = $this.attr( 'href' );
                 var query = marketpress.unserialize( href );
-
                 $form.find( 'input[name="page"]' ).val( query.paged );
-                marketpress.updateProductList();
+                marketpress.updateProductList( );
             } );
         },
         /**
@@ -315,18 +296,16 @@ var marketpress = { };
          * @since 3.0
          * @access public
          */
-        updateProductList: function() {
+        updateProductList: function( ) {
             var $form = $( '#mp_product_list_refine' );
-            var data = $form.serialize();
+            var data = $form.serialize( );
             var url = mp_i18n.ajaxurl + '?action=mp_update_product_list';
-
             marketpress.loadingOverlay( 'show' );
-
             $.post( url, data ).done( function( resp ) {
                 marketpress.loadingOverlay( 'hide' );
-                $( '#mp_product_nav' ).remove();
+                $( '#mp_product_nav' ).remove( );
                 $( '#mp_product_list' ).replaceWith( resp );
-                mp_cart.initCartListeners();
+                mp_cart.initCartListeners( );
             } );
         },
         /**
@@ -344,12 +323,10 @@ var marketpress = { };
 
             var dataPairs = str.split( '&' );
             var obj = { };
-
             $.each( dataPairs, function( index, value ) {
                 var tmp = value.split( '=' );
                 obj[ tmp[0] ] = tmp[1];
             } );
-
             return obj;
         },
         /**
@@ -357,12 +334,11 @@ var marketpress = { };
          *
          * @since 3.0
          */
-        initCreateAccountLightboxListeners: function() {
+        initCreateAccountLightboxListeners: function( ) {
             var $lb = $( '#mp-create-account-lightbox' );
             var $emailLabel = $( 'label[for="mp-create-account-email"]' );
             var $emailInput = $( '#mp-create-account-email' );
             var $submitButton = $lb.find( ':submit' );
-
             if ( '#mp-create-account' != window.location.hash || 0 == $lb.length ) {
                 // Bail
                 return false;
@@ -381,7 +357,6 @@ var marketpress = { };
                 $emailInput.prop( 'disabled', true );
                 $submitButton.prop( 'disabled', true );
             } );
-
             $( document ).ajaxComplete( function( evt, jqxhr, settings ) {
                 if ( settings.url.indexOf( 'action=mp_check_if_email_exists' ) < 0 ) {
                     return;
@@ -391,35 +366,31 @@ var marketpress = { };
                 $emailInput.prop( 'disabled', false );
                 $submitButton.prop( 'disabled', false );
             } );
-
             $lb.find( 'form' ).validate( {
-                highlight: function() {
-                    setTimeout( function() {
-                        $.colorbox.resize();
+                highlight: function( ) {
+                    setTimeout( function( ) {
+                        $.colorbox.resize( );
                     }, 100 )
                 },
                 onkeyup: false, // don't validate on keyup as this will send ajax requests on every key stroke!
                 submitHandler: function( form ) {
                     var $form = $( form );
-
                     marketpress.loadingOverlay( 'show' );
-
-                    $.post( $form.attr( 'action' ), $form.serialize() ).done( function( resp ) {
+                    $.post( $form.attr( 'action' ), $form.serialize( ) ).done( function( resp ) {
                         if ( resp.success ) {
-                            window.location.reload();
+                            window.location.reload( );
                         } else {
                             marketpress.loadingOverlay( 'hide' );
                             alert( resp.data.message );
                         }
                     } );
                 },
-                unhighlight: function() {
-                    setTimeout( function() {
-                        $.colorbox.resize();
+                unhighlight: function( ) {
+                    setTimeout( function( ) {
+                        $.colorbox.resize( );
                     }, 100 )
                 }
             } );
-
             $.colorbox( {
                 close: "x",
                 escKey: false,
@@ -431,17 +402,15 @@ var marketpress = { };
         }
     };
 }( jQuery ) );
-
-jQuery( document ).ready( function() {
-    marketpress.initSelect2();
-    marketpress.initProductTabs();
-    marketpress.initToolTips();
-    marketpress.initOrderLookup();
-    marketpress.initImageLightbox();
-    marketpress.initProductFiltersPagination();
-    marketpress.initCreateAccountLightboxListeners();
+jQuery( document ).ready( function( ) {
+    marketpress.initSelect2( );
+    marketpress.initProductTabs( );
+    marketpress.initToolTips( );
+    marketpress.initOrderLookup( );
+    marketpress.initImageLightbox( );
+    marketpress.initProductFiltersPagination( );
+    marketpress.initCreateAccountLightboxListeners( );
 } );
-
-window.onload = function() {
-    marketpress.equalizeProductGrid();
+window.onload = function( ) {
+    marketpress.equalizeProductGrid( );
 }
