b4a74c20bafdf7c320e2e91e71a1e949929cac71
Marko Miljus <marko@incsub.com>
2015-08-19 13:20:14 +0200

    Fixed issues with slashed in the text area WPMUDev field element

--- includes/wpmudev-metaboxes/fields/class-wpmudev-field-textarea.php
+++ includes/wpmudev-metaboxes/fields/class-wpmudev-field-textarea.php
@@ -1,6 +1,7 @@
 <?php
 
 class WPMUDEV_Field_Textarea extends WPMUDEV_Field {
+
 	/**
 	 * Displays the field
 	 *
@@ -9,9 +10,11 @@ class WPMUDEV_Field_Textarea extends WPMUDEV_Field {
 	 * @param int $post_id
 	 */
 	public function display( $post_id ) {
-		$this->before_field(); ?>
-		<textarea <?php echo $this->parse_atts(); ?>><?php echo esc_textarea($this->get_value($post_id)); ?></textarea>
+		$this->before_field();
+		?>
+		<textarea <?php echo $this->parse_atts(); ?>><?php echo esc_textarea( stripslashes( $this->get_value( $post_id ) ) ); ?></textarea>
 		<?php
 		$this->after_field();
 	}
+
 }
