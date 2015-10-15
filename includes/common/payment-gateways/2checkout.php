d100099428634ffdde5b7468ef0291fe0c5ad516
JulienZ <julien@incsub.com>
2015-10-15 13:50:34 +0200

    Tax cannot be shown on 2CO if tax is inclusive

--- includes/common/payment-gateways/2checkout.php
+++ includes/common/payment-gateways/2checkout.php
@@ -219,12 +219,7 @@ class MP_Gateway_2Checkout extends MP_Gateway_API {
 		$items = $cart->get_items_as_objects();
 		
 		foreach ( $items as $item ) {
-			if ( mp_get_setting( 'tax->tax_inclusive' ) ) {
-				$price = round( $item->before_tax_price(), 2 );
-			}
-			else {
-				$price = $item->get_price( 'lowest' );
-			}
+			$price = $item->get_price( 'lowest' );
 			
 			$total += ($price * $item->qty);
 			
@@ -258,7 +253,7 @@ class MP_Gateway_2Checkout extends MP_Gateway_API {
 		}
 
 		//tax line
-		// if ( ! mp_get_setting( 'tax->tax_inclusive' ) ) {
+		if ( ! mp_get_setting( 'tax->tax_inclusive' ) ) {
 			$tax_price = $cart->tax_total( false );
 			$prefix = 'li_' . $counter;
 			$params["{$prefix}_product_id"] = 'taxes';
@@ -268,12 +263,9 @@ class MP_Gateway_2Checkout extends MP_Gateway_API {
 			
 			$counter += 1;
 			$total += $tax_price;
-		// }
+		}
 
 		$params['total'] = $total;
-
-		// print_r( $params );
-		// die;
 		
 		$url .= '?' . http_build_query( $params );
 		
