f3a3b6b7416ad3d8fdc5f0f0082d53c9e6747159
Ricardo Freitas <ricardotbfreitas@gmail.com>
2016-05-26 11:39:08 +0100

    - Add new grunt tasks to handle sass and css (for now only for core styles shown on FE) - Update POT - Remove unused/old MP .less files - Update Jeet Framework - Fix conflicts with Jeet mixing - New folders/files organization for sass (src)

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,66 @@
 /*global module, require */
 module.exports = function(grunt) {
+
+	// Load Grunt tasks
+	grunt.loadNpmTasks('grunt-postcss');
+	grunt.loadNpmTasks('grunt-sass');
 	grunt.loadNpmTasks('grunt-wp-i18n');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
 	grunt.initConfig({
+
+		// Read package meta data
+		pkg: grunt.file.readJSON('package.json'),
+
+		// Project variables
+		project: {
+			assets: 'ui',
+			src: '<%= project.assets %>/src',
+			fcss: '<%= project.assets %>/css',
+			fscss: '<%= project.src %>/scss/front',
+			tcss: '<%= project.assets %>/themes',
+			tscss: '<%= project.src %>/scss/themes'
+		},
+
+		// PostCSS
+		postcss: {
+            options: {
+                processors: [
+                    require('autoprefixer')({
+                        browsers: ['> 5%']['last 2 versions']
+                    })
+                ]
+            },
+            frontprod: {
+                src: '<%= project.fcss %>/marketpress.css'
+            }
+        },
+
+        // SASS
+		sass: {
+			frontdev: {
+				files: {
+					'<%= project.fcss %>/marketpress.css': '<%= project.fscss %>/marketpress.scss'
+				},
+				options: {
+					sourceMap: true,
+					outputStyle: 'expanded',
+					imagePath: 'images/'
+				}
+			},
+			frontprod: {
+				files: {
+					'<%= project.fcss %>/marketpress.css': '<%= project.fscss %>/marketpress.scss'
+				},
+				options: {
+					sourceMap: false,
+					outputStyle: 'compressed',
+					imagePath: 'images/'
+				}
+			}
+		},
+
+		// Make POT
 		makepot: {
 			target: {
 				options: {
@@ -11,7 +69,31 @@ module.exports = function(grunt) {
 				}
 			}
 		},
+
+		// Watch
+		watch: {
+			sass: {
+				files: '<%= project.src %>/scss/{,*/}*.{scss,sass}',
+				tasks: ['sass:frontdev']
+			}
+		},
+
 	});
 
-	grunt.registerTask('default', ['makepot']);
+	// Grunt default task (run `grunt`)
+	grunt.registerTask('default', [
+		'watch'
+	]);
+
+	grunt.registerTask('ccss', [
+		'sass:frontprod',
+		'postcss:frontprod'
+	]);
+
+	grunt.registerTask('release', [
+		'sass:frontprod',
+		'postcss:frontprod',
+		'makepot'
+	]);
+
 };
