f1bc0618102645d17f106da6df3e2c6bf66d85b6
Cvetan <cvetan@incsub.com>
2016-11-21 07:25:36 +0200

    Fix fatal error when merchan_email exist

--- includes/common/payment-gateways/paypal-express.php
+++ includes/common/payment-gateways/paypal-express.php
@@ -383,7 +383,8 @@ class MP_Gateway_Paypal_Express extends MP_Gateway_API {
 
 			if ( $cart->is_global ) {
 				//check if the current merchant don't have email setting, we bypass
-				$merchant_email = mp_get_network_setting( 'gateways' )['paypal_express']['merchant_email'];
+				$gateways = mp_get_network_setting( 'gateways' );
+				$merchant_email = $gateways['paypal_express']['merchant_email'];
 				$merchant_email = trim( $merchant_email );
 				if ( empty( $merchant_email ) || strlen( $merchant_email ) == 0 ) {
 					continue;
